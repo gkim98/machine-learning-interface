52b2fba4c4e430aab456ff10702dc70caf8cc3df
Grant Kim <grantkim93@gmail.com>
2018-07-06 00:59:29 -0400

    combine index and app files

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+import dash_core_components as dcc   
+import dash_html_components as html 
+from dash.dependencies import Input, Output 
+
+from app import app 
+from components import file_uploader
+from components import create_dropdowns, algorithm_radio, slider_holder, subset_checkbox
+from components import run_button
+from components import metrics_graph, marker_info
+from components import results_holder, download_button
+import callbacks
+
 import dash 
 
 app = dash.Dash(__name__)
@@ -5,3 +17,33 @@ server = app.server
 app.scripts.config.serve_locally=True
 app.config.suppress_callback_exceptions=True 
 
+app.layout = html.Div([
+    file_uploader,
+    html.Div(id='dropdown-holder', children=create_dropdowns(), 
+        style=dict(width='100%')),
+    html.Div([
+        algorithm_radio, subset_checkbox
+    ], style=dict(
+        display='flex',
+        justifyContent='space-between',
+        marginTop='10px'
+    )),
+    html.Div([
+        slider_holder, run_button
+    ], style=dict(marginTop='20px')),
+    html.Div([
+        metrics_graph, marker_info
+    ], style=dict(
+        width='100%', 
+        display='flex',
+        alignItems='flex-start'
+    )),
+    download_button,
+
+
+    results_holder
+    
+], style=dict(width='100%'))
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
